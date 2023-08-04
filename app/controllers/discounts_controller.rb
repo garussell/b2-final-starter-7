72a9dc423f44a1d74f3d56a867c20c1d1def391d
Allen Russell <garyrusselljr@gmail.com>
2023-08-04 09:32:31 -0600

    feat: Add create discount method and tested for flash errors

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -6,4 +6,27 @@ class DiscountsController < ApplicationController
   def show
     # require 'pry';binding.pry
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.new
+  end
+
+  def create 
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.new(discount_params)
+
+    if @discount.save
+      flash[:success] = "Discount was created, very generous."
+      redirect_to merchant_discounts_path(@merchant)
+    else
+      flash[:error] = "Invalid entry, try again."
+      render :new
+    end
+  end
+
+  private
+  def discount_params
+    params.require(:discount).permit(:name, :discount_quantity, :discount_percentage)
+  end
 end
