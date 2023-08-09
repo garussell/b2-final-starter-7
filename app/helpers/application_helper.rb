6e51c4dafa827dd6a859543456303274b0065277
Allen Russell <garyrusselljr@gmail.com>
2023-08-09 07:46:58 -0600

    fix: toggle button and view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,19 @@
 module ApplicationHelper
+#   def holiday_discount_urls(merchant, holiday)
+#     discount = Discount.find_id(holiday.name)
+    
+#     if discount && Discount.discount_exists?(holiday.name)
+#       link_to "View Discount", merchant_discount_path(merchant, discount), data: { turbo: false }
+#     else
+#       button_to "Create #{holiday.name} Discount", new_merchant_discount_path(merchant), method: :get, data: { turbo: false }
+#     end
+#   end
+# end
+
   def holiday_discount_urls(merchant, holiday)
     discount = Discount.find_id(holiday.name)
-    
-    if discount && Discount.discount_exists?(holiday.name)
-      link_to "View Discount", merchant_discount_path(merchant, discount), data: { turbo: false }
+    if Discount.discount_exists?(holiday.name)
+      link_to "View Discount", url: "/merchants/#{@merchant.id}/discounts/#{discount}" 
     else
       button_to "Create #{holiday.name} Discount", new_merchant_discount_path(merchant), method: :get, data: { turbo: false }
     end
